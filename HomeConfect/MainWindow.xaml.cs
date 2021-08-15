593f5a2cbda81a8e7a488daa388a079ac6c3f530
Софья Качулина <sof.gilyazeva@yandex.ru>
2021-08-15 15:42:19 +0500

    Changed nav menu. Added material design package.

--- HomeConfect/MainWindow.xaml.cs
+++ HomeConfect/MainWindow.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using HomeConfect.Domain.Criterion;
+using HomeConfect.Domain.Services.Recipes;
 using HomeConfect.Storage;
 using HomeConfect.Storage.Queries;
 using HomeConfect.ViewModels;
-using System;
-using System.Collections.Generic;
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HomeConfect
 {
@@ -26,23 +17,69 @@ namespace HomeConfect
     {
         private readonly Context dataContext;
 
-        public MainWindow(Context context)
+        public MainWindow(Context context, IRecipeService recipeService)
         {
             InitializeComponent();
 
             dataContext = context;
 
             // TODO ?
-            DataContext = new MainViewModel
+            DataContext = new MainViewModel(recipeService)
             {
-                RecipeBookVM = new RecipeBookViewModel
+                RecipeBookVM = new RecipeBookViewModel(recipeService)
                 {
                     CurrentView = new RecipeTilesFeedViewModel
                     {
-                        RecipeTiles = new GetAllRecipesQuery(context).Ask(new GetAllRecipes()).Select(x => new RecipeTileViewModel(x)).ToList()
+                        RecipeTiles = new GetAllRecipesQuery(context)
+                            .Ask(new GetAllRecipes())
+                            .Select(x => new RecipeTileViewModel(x))
+                            .ToList(),
+                        Recipes = context.Recipes.Local.ToObservableCollection()
                     }
                 }
             };
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                ttRecipeBook.Visibility = Visibility.Collapsed;
+                ttCalendar.Visibility = Visibility.Collapsed;
+                ttShopingList.Visibility = Visibility.Collapsed;
+                ttSettings.Visibility = Visibility.Collapsed;
+                ttSignout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ttRecipeBook.Visibility = Visibility.Visible;
+                ttCalendar.Visibility = Visibility.Visible;
+                ttShopingList.Visibility = Visibility.Visible;
+                ttSettings.Visibility = Visibility.Visible;
+                ttSignout.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //img_bg.Opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            //img_bg.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
---
+++ HomeConfect/Views/Controls/MenuButton.xaml.cs
@@ -0,0 +1,60 @@
+﻿using HomeConfect.Core;
+
+using MaterialDesignThemes.Wpf;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HomeConfect.Views.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuButton.xaml
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PackIconKind), typeof(MenuButton));
+
+        public PackIconKind Icon
+        {
+            get => (PackIconKind)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MenuButton));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(MenuButton));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(
+                "Command",
+                typeof(RelayCommand),
+                typeof(MenuButton),
+                new UIPropertyMetadata(null));
+
+        public RelayCommand Command
+        {
+            get => (RelayCommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
